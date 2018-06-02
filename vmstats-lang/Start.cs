95ba07992de20dd7a4ccb7a67796036a91dc67c6
jon catlin <jcatlin@concordservicing.com>
2018-06-01 17:26:27 -0700

    Started to write a DSL for the transforms. Need to determine how to parse a line of DSL and then walk it with ANTLR. See previous Java project for details.

--- vmstats-lang/Start.cs
+++ vmstats-lang/Start.cs
@@ -7,7 +7,6 @@ namespace vmstats.lang
 {
     public class Start
     {
-/*
         private static void Main(string[] args)
         {
             try
@@ -17,29 +16,43 @@ namespace vmstats.lang
                 Console.WriteLine("The grammer to be parsed");
 
                 // to type the EOF character and end the input: use CTRL+D, then press <enter>     
-                while ((input = Console.ReadLine()) != "\u0004")
-                {
-                    text.AppendLine(input);
-                }
+                /*
+                                while ((input = Console.ReadLine()) != "\u0004")
+                                {
+                                    text.AppendLine(input);
+                                }
+                */
+                input = Console.ReadLine();
 
                 AntlrInputStream inputStream = new AntlrInputStream(text.ToString());
 
-                LanguageLexer languageLexer = new LanguageLexer(inputStream);
-                CommonTokenStream commonTokenStream = new CommonTokenStream(languageLexer);
-                LanguageParser languageParser = new LanguageParser(commonTokenStream);
-                LanguageParser.StatementContext context = languageParser.statement();
+                vmstatsLexer myLexer = new vmstatsLexer(inputStream);
+                CommonTokenStream commonTokenStream = new CommonTokenStream(myLexer);
+                vmstatsParser myParser = new vmstatsParser(commonTokenStream);
+
+                vmstatsParser.Transform_pipelineContext transform_pipelineContext = myParser.transform_pipeline();
+
+                int i = 0;
+/*
+                myParser.StatementContext context = myParser.statement();
                 LanguageVisitor visitor = new LanguageVisitor();
                 visitor.Visit(context);
                 foreach (var line in visitor.Lines)
                 {
                     Console.WriteLine("{0} has said \"{1}\"", line.Person, line.Text);
                 }
+*/
             }
             catch (Exception ex)
             {
                 Console.WriteLine("Error: " + ex);
             }
         }
-        */
+    }
+
+
+    public class MyVisitor : vmstatsBaseVisitor<object>
+    {
+        public List<VisitTransform_pipeline>
     }
 }
